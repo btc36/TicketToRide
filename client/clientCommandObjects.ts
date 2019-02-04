798eda2722fc9314f10ea06486502bb2428b8a26
Brennah Oaks <brennah.oak@gmail.com>
2019-02-04 14:24:14 -0700

    adding client?

---
+++ client/clientCommandObjects.ts
@@ -0,0 +1,12 @@
+class ClientCommandObjects {
+    className: string;
+    methodName: string;
+    paramTypes: [];
+    paramValues: [];
+    constructor(public classNameIn: string, public methodNameIn: string, public paramTypesIn: [], public paramValuesIn: []) {
+        this.className = classNameIn;
+        this.methodName = methodNameIn;
+        this.paramTypes = paramTypesIn;
+        this.paramValues = paramValuesIn;
+    }
+}
