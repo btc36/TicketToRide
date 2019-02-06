b1f3ec839bfad7e57cc78ea24d63b5ebec6826de
Brennah Oaks <brennah.oak@gmail.com>
2019-02-06 14:36:34 -0700

    finishing proxy

--- client/serverProxy.ts
+++ client/serverProxy.ts
@@ -1,4 +1,5 @@
 import { ClientCommandObjects } from "./clientCommandObjects";
+//import { clientCommunicator } from "./clientCommumnicator"
 
 class ServerProxy {
     host: string;
@@ -8,21 +9,39 @@ class ServerProxy {
         this.port = portIn;
     }
     public register(username:string, password:string, confirm:string){
-        let url = new URL("http://" + this.host + ":" + this.port + "/user/register");
+        let url = new URL("http://" + this.host + ":" + this.port + "/register");
+        let command = new ClientCommandObjects("ServerFacade", "register", ["string", "string"], [username, password]);
+        let clientCommunicator = new clientCommunicator(url, this.port);
+        clientCommunicator.sendCommand(command);
     }
     public login(username: string, password: string){
-
+        let url = new URL("http://" + this.host + ":" + this.port + "/login");
+        let command = new ClientCommandObjects("ServerFacade", "login", ["string", "string"], [username, password]);
+        let clientCommunicator = new clientCommunicator(url, this.port);
+        clientCommunicator.sendCommand(command);
     }
-    public createGame(player: Player, numPlayers: number, gameName: string){
-
+    public createGame(username: string, numPlayers: number, gameName: string){
+        let url = new URL("http://" + this.host + ":" + this.port + "/creategame");
+        let command = new ClientCommandObjects("ServerFacade", "createGame", ["string", "string", "number"], [username, gameName, numPlayers]);
+        let clientCommunicator = new clientCommunicator(url, this.port);
+        clientCommunicator.sendCommand(command);
     }
-    public joinGame(player: Player, gameId: number){
-
+    public joinGame(username: string, gameName: string, gameId: string){
+        let url = new URL("http://" + this.host + ":" + this.port + "/joingame");
+        let command = new ClientCommandObjects("ServerFacade", "joinGame", ["string", "string", "string"], [username, gameName, gameId]);
+        let clientCommunicator = new clientCommunicator(url, this.port);
+        clientCommunicator.sendCommand(command);
     }
-    public startGame(player: Player, gameId: number){
-
+    public startGame(gameId: string){
+        let url = new URL("http://" + this.host + ":" + this.port + "/startgame");
+        let command = new ClientCommandObjects("ServerFacade", "startGame", ["string"], [gameId]);
+        let clientCommunicator = new clientCommunicator(url, this.port);
+        clientCommunicator.sendCommand(command);
     }
     public getGameList(){
-
+        let url = new URL("http://" + this.host + ":" + this.port + "/getgamelist");
+        let command = new ClientCommandObjects("ServerFacade", "getGameList", [], []);
+        let clientCommunicator = new clientCommunicator(url, this.port);
+        clientCommunicator.sendCommand(command);
     }
 }
