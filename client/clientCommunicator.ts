ce586517d9fda81247eb9778f60e370bce87efcd
Brennah Oaks <brennah.oak@gmail.com>
2019-02-08 16:43:24 -0700

    all Brennah's changes

--- client/clientCommunicator.ts
+++ client/clientCommunicator.ts
@@ -1,16 +1,37 @@
 import { ClientCommandObjects } from "./clientCommandObjects";
-//import { Serializer } from "./serializer";
+import { Serializer } from "./Serializer";
 
 export class ClientCommunicator {
-    serverUrl: URL;
+    serverUrl: string;
     serverPort: string;
-    constructor(public serverUrlIn: URL, public serverPortIn: string) {
+    serializer: Serializer;
+    constructor(public serverUrlIn: string, public serverPortIn: string) {
         this.serverUrl = serverUrlIn;
         this.serverPort = serverPortIn;
+        this.serializer = new Serializer();
     }
     public sendCommand(command: ClientCommandObjects){
-
+        var data = this.serializer.toJSON(command);
+        var request = new XMLHttpRequest();
+        request.open('POST', this.serverUrl, true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.onload = function() {
+            var currentSerializer = new Serializer();
+            if (request.status >= 200 && request.status < 400) {
+              // Success!
+              var result = currentSerializer.parseJSON(request.responseText);
+            } else {
+              // We reached our target server, but it returned an error
+          
+            }
+          };
+          
+          request.onerror = function() {
+            // There was a connection error of some sort
+          };
+        request.send(data);
     }
+
     public executeCommands(commands: ClientCommandObjects[]){
 
     }
--- client/serverProxy.ts
+++ client/serverProxy.ts
@@ -1,5 +1,5 @@
-import { ClientCommandObjects } from "./clientCommandObjects";
-import { ClientCommunicator } from "./clientCommunicator";
+import { ClientCommandObjects } from "./ClientCommandObjects";
+import { ClientCommunicator } from "./ClientCommunicator";
 //import { Serializer } from "./serializer";
 
 export class ServerProxy {
@@ -10,39 +10,39 @@ export class ServerProxy {
         this.port = portIn;
     }
     public register(username:string, password:string, confirm:string){
-        let url = new URL("http://" + this.host + ":" + this.port + "/register");
-        let command = new ClientCommandObjects("ServerFacade", "register", ["string", "string"], [username, password]);
-        let communicator = new ClientCommunicator(url, this.port);
+        var url = "http://" + this.host + ":" + this.port + "/register";
+        var command = new ClientCommandObjects("ServerFacade", "register", ["string", "string"], [username, password]);
+        var communicator = new ClientCommunicator(url, this.port);
         communicator.sendCommand(command);
     }
     public login(username: string, password: string){
-        let url = new URL("http://" + this.host + ":" + this.port + "/login");
-        let command = new ClientCommandObjects("ServerFacade", "login", ["string", "string"], [username, password]);
-        let communicator = new ClientCommunicator(url, this.port);
+        var url = "http://" + this.host + ":" + this.port + "/login";
+        var command = new ClientCommandObjects("ServerFacade", "login", ["string", "string"], [username, password]);
+        var communicator = new ClientCommunicator(url, this.port);
         communicator.sendCommand(command);
     }
     public createGame(username: string, numPlayers: number, gameName: string){
-        let url = new URL("http://" + this.host + ":" + this.port + "/creategame");
-        let command = new ClientCommandObjects("ServerFacade", "createGame", ["string", "string", "number"], [username, gameName, numPlayers]);
-        let communicator = new ClientCommunicator(url, this.port);
+        var url = "http://" + this.host + ":" + this.port + "/creategame";
+        var command = new ClientCommandObjects("ServerFacade", "createGame", ["string", "string", "number"], [username, gameName, numPlayers]);
+        var communicator = new ClientCommunicator(url, this.port);
         communicator.sendCommand(command);
     }
     public joinGame(username: string, gameName: string, gameId: string){
-        let url = new URL("http://" + this.host + ":" + this.port + "/joingame");
-        let command = new ClientCommandObjects("ServerFacade", "joinGame", ["string", "string", "string"], [username, gameName, gameId]);
-        let communicator = new ClientCommunicator(url, this.port);
+        var url = "http://" + this.host + ":" + this.port + "/joingame";
+        var command = new ClientCommandObjects("ServerFacade", "joinGame", ["string", "string", "string"], [username, gameName, gameId]);
+        var communicator = new ClientCommunicator(url, this.port);
         communicator.sendCommand(command);
     }
     public startGame(gameId: string){
-        let url = new URL("http://" + this.host + ":" + this.port + "/startgame");
-        let command = new ClientCommandObjects("ServerFacade", "startGame", ["string"], [gameId]);
-        let communicator = new ClientCommunicator(url, this.port);
+        var url = "http://" + this.host + ":" + this.port + "/startgame";
+        var command = new ClientCommandObjects("ServerFacade", "startGame", ["string"], [gameId]);
+        var communicator = new ClientCommunicator(url, this.port);
         communicator.sendCommand(command);
     }
     public getGameList(){
-        let url = new URL("http://" + this.host + ":" + this.port + "/getgamelist");
-        let command = new ClientCommandObjects("ServerFacade", "getGameList", [], []);
-        let communicator = new ClientCommunicator(url, this.port);
+        var url = "http://" + this.host + ":" + this.port + "/getgamelist";
+        var command = new ClientCommandObjects("ServerFacade", "getGameList", [], []);
+        var communicator = new ClientCommunicator(url, this.port);
         communicator.sendCommand(command);
     }
 }
