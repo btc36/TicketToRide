847714be126d0045947192a04a6502779750588b
janders111 <jordananders1@gmail.com>
2019-02-11 14:41:36 -0700

    Now the server can serve the other web files besides index.html

--- server/communication/WebpageHandler.java
+++ server/communication/WebpageHandler.java
@@ -13,24 +13,30 @@ import com.sun.net.httpserver.*;
     class WebpageHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String filePathStr = ".." + File.separator + "client" + File.separator + "index.html"; //location of the html file
             Path filePath;
             OutputStream respBody = null;
 
-            System.out.println(new File("").getAbsolutePath()); // print the current working directory
+            //System.out.println(new File("").getAbsolutePath()); // print the current working directory
 
             try {
                 if (exchange.getRequestMethod().toLowerCase().equals("get")) {
+                    URI uri = exchange.getRequestURI();
+                    String filePathStr = ".." + File.separator + "client" + File.separator;
                     System.out.println("Recieved a GET request for the homepage");
 
+                    if(uri.toString().equals("/")) {
+                        uri = URI.create("/index.html");
+                    }
                     exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                    filePathStr += uri.toString();
 
                     //check to see if file exists.
                     File tmpDir = new File(filePathStr);
                     if(!tmpDir.exists()) {
                         //if file does not exist, change path to that of the 404 page.
-                        filePathStr = "web" + File.separator + "404.html";
+                        filePathStr += "404.html";
                     }
+                    tmpDir = null; //make sure it is not accessed again
 
                     try {
                         filePath = FileSystems.getDefault().getPath(filePathStr);
