20b84758c337a31111ab72f0a20540eb73b10e7c
Jake <ilovejava95@gmail.com>
2019-03-09 15:42:23 -0700

    game start works on server

--- server/server/Phase2Tester.java
+++ server/server/Phase2Tester.java
@@ -12,24 +12,25 @@ public class Phase2Tester
 {
     public static void main(String[] args)
     {
-//        String divider = "----------------------------------------------";
-//        ServerFacade facade = new ServerFacade();
-//
-//        facade.register("user1", "user1");
-//        facade.register("user2", "user2");
-//        facade.register("user3", "user3");
-//        facade.register("user4", "user4");
-//        System.out.println(divider);
-//        facade.login("user1", "user1");
-//        facade.login("user2", "user2");
-//        facade.login("user3", "user3");
-//        facade.login("user4", "user4");
-//        System.out.println(divider);
-//        facade.createGame("user1","game1","4");
-//        facade.joinGame("user2", "game1");
-//        facade.joinGame("user3", "game1");
-//        facade.joinGame("user4", "game1");
-//        System.out.println(divider);
+        String divider = "----------------------------------------------";
+        ServerFacade facade = new ServerFacade();
+
+        facade.register("user1", "user1");
+        facade.register("user2", "user2");
+        facade.register("user3", "user3");
+        facade.register("user4", "user4");
+        System.out.println(divider);
+        facade.login("user1", "user1");
+        facade.login("user2", "user2");
+        facade.login("user3", "user3");
+        facade.login("user4", "user4");
+        System.out.println(divider);
+        facade.createGame("user1","game1","4");
+        facade.joinGame("user2", "game1");
+        facade.joinGame("user3", "game1");
+        facade.joinGame("user4", "game1");
+        System.out.println(divider);
+        facade.startGame("game1");
 //        Date date = new Date();
 //        facade.sendChat("HELLO", date,"user1", "game1" );
 //        facade.sendChat("WORLD", date,"user2", "game1" );
