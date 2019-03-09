2d0195553420c53a33d2981a582439252d53bd6c
Jake <ilovejava95@gmail.com>
2019-03-08 20:49:54 -0700

    update on Playerinfo

--- client/src/Views/PlayerInfoView.tsx
+++ client/src/Views/PlayerInfoView.tsx
@@ -2,9 +2,39 @@ import * as React from "react";
 import * as I from "../ViewModels/IPlayerInfoViewModel";
 
 export const PlayerInfoView  = (component: I.IPlayerInfoViewModel) => {
+
+    const players = new Array<any>();
+    const playerList = component.state.playerList
+    const cards = new Array<any>();
+    let cardList;
+    let turn = false;
+    for (let i = 0; i < playerList.length; i++) {
+        if(component.state.username == playerList[i].username)
+        {
+            turn = playerList[i].myTurn;
+        }
+        players.push(
+            <li>{playerList[i].username} : {playerList[i].score} {playerList[i].numTrainCards} {playerList[i].numDestinationCards}</li>
+        //numTrainCards: number;
+        //numDestinationCards: number;
+        );
+    }
   return (
-    <div>
-      Hello
-    </div>
+      <div>
+          <div>
+              <p><b><u>My Info</u></b></p>
+              <ul>
+                  Myturn : {turn}
+              </ul>
+
+          </div>
+          <div>
+              <p><b><u>Player Info</u></b></p>
+              <ul>
+                  {players}
+              </ul>
+              <p><b>Message</b></p>
+          </div>
+      </div>
   );
 }
