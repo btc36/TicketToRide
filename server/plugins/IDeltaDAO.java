b2db9b8616f370aaedf5945b9a9cdb0c00623050
Lincoln Bergeson <lincolnbergeson@gmail.com>
2019-04-10 14:42:57 -0600

    added init function to dao interfaces

--- server/plugins/IDeltaDAO.java
+++ server/plugins/IDeltaDAO.java
@@ -3,6 +3,7 @@ package plugins;
 public interface IDeltaDAO {
     String dbFilePath = "delta.db";
 
+    public void init();
     public void clear();
     public void addDelta();
     public IDeltaDAO getDelta();
--- server/plugins/ISnapshotDAO.java
+++ server/plugins/ISnapshotDAO.java
@@ -3,6 +3,7 @@ package plugins;
 public interface ISnapshotDAO {
     String dbFilePath = "snapshot.db";
 
+    public void init();
     public void clear();
     public void updateSnapshot();
     public ISnapshotDAO getLatestSnapshot();
