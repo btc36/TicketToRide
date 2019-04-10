23abada9654fab4e1cb0fd142e49b5ecd0f151fc
janders111 <jordananders1@gmail.com>
2019-04-10 14:45:55 -0600

    i

---
+++ server/plugins/FileDB/FileSnapshotDAO.java
@@ -0,0 +1,31 @@
+package plugins.FileDB;
+import plugins.ISnapshotDAO;
+
+import java.io.File;
+
+public class FileSnapshotDAO implements ISnapshotDAO {
+
+    @Override
+    public void clear() {
+        File file = new File(dbFilePath);
+        if(file.delete())
+        {
+            System.out.println("File deleted successfully");
+        }
+        else
+        {
+            System.out.println("Failed to delete the file");
+        }
+    }
+
+    @Override
+    public void updateSnapshot() {
+        //File file = new File(dbFilePath);
+        //os.writeObject(model.ServerModel.getInstance());
+    }
+
+    @Override
+    public ISnapshotDAO getLatestSnapshot() {
+        return null;
+    }
+}
