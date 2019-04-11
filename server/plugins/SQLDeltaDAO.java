12037065e0fd5db9b78b59f26c55dc89449abdd3
Jake <ilovejava95@gmail.com>
2019-04-11 00:15:10 -0600

    .

--- server/plugins/SQLDeltaDAO.java
+++ server/plugins/SQLDeltaDAO.java
@@ -1,8 +1,25 @@
 package plugins;
 
+import java.sql.*;
+import java.util.logging.Logger;
+
+import static java.lang.System.exit;
+
 public class SQLDeltaDAO implements IDeltaDAO
 {
+    private static Logger logger;
+    static
+    {
+        logger = Logger.getLogger("SQLDeltaLog");
+    }
+
     String databaseFilepath = "jdbc:sqlite:ttr.sqlite";
+
+    private Connection conn;
+    private String dbName;
+    private ResultSet rs;
+    private PreparedStatement pstmt;
+    private Statement stmt;
 
     public SQLDeltaDAO()
     {
@@ -18,23 +35,133 @@ public class SQLDeltaDAO implements IDeltaDAO
     @Override
     public void clear()
     {
-
+        boolean success = false;
+        try
+        {
+            String sql = "DELETE FROM DELTA";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            success = true;
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeStatements();
+        }
     }
 
     @Override
     public void addDelta(Object object)
     {
-
+        createTable();
+        openConnection();
+        closeConnection(true);
     }
     @Override
-    public Object getDelta()
+    public Object[] getAllDelta()
     {
 
         return null;
     }
 
-    private void addDelta(String s)
+    private void createTable()
     {
-
+        try
+        {
+            String sql = "CREATE TABLE IF NOT EXISTS DELTA (\n"
+                    + "	id text NOT NULL PRIMARY KEY,\n"
+                    + "	gameDelta blob NOT NULL\n"
+                    + ");";
+            stmt = conn.createStatement();
+            stmt.execute(sql);
+            System.out.println("table created\n");
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeStatements();
+        }
     }
+
+    private void openConnection()
+    {
+        conn = null;
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(databaseFilepath);
+            conn.setAutoCommit(false);
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+            exit(0);
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Error occurred while opening connection");
+            e.printStackTrace();
+            exit(0);
+        }
+    }
+
+    private void closeConnection(boolean commit)
+    {
+        logger.entering("SQLDeltaDAO", "closeConnection");
+        try
+        {
+            if(conn!=null)
+            {
+                if(commit)
+                {
+                    System.out.println("Saving Database");
+                    conn.commit();
+                }
+                else
+                    conn.rollback();
+                conn.close();
+                conn = null;
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error occurred while closing connection");
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        logger.exiting("SQLDeltaDAO", "closeConnection");
+    }
+
+    private void closeStatements()
+    {
+        try
+        {
+            if(rs!=null) {
+                rs.close();
+                rs = null;
+            }
+            if(pstmt!=null)
+            {
+                pstmt.close();
+                pstmt = null;
+            }
+            if(stmt!=null)
+            {
+                stmt.close();
+                stmt = null;
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error occurred while closing connection");
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            exit(0);
+        }
+    }
+
 }
--- server/plugins/SQLSnapshotDAO.java
+++ server/plugins/SQLSnapshotDAO.java
@@ -1,25 +1,101 @@
 package plugins;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.logging.Logger;
 
 import static java.lang.System.exit;
 
 public class SQLSnapshotDAO implements ISnapshotDAO
 {
+    private static Logger logger;
+    static
+    {
+        logger = Logger.getLogger("SQLSnapLog");
+    }
+
     String databaseFilepath = "jdbc:sqlite:ttr.sqlite";
     private Connection conn;
     private String dbName;
+    private ResultSet rs;
+    private PreparedStatement pstmt;
+    private Statement stmt;
 
     public SQLSnapshotDAO()
     {
-
+        this.conn = null;
+        this.rs = null;
+        this.pstmt = null;
+        this.stmt = null;
     }
 
     @Override
     public void init()
     {
+
+    }
+
+    @Override
+    public void clear()
+    {
+        boolean success = false;
+        try
+        {
+            String sql = "DELETE FROM SNAPSHOT";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            success = true;
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeStatements();
+        }
+    }
+
+    @Override
+    public void updateSnapshot(Object object)
+    {
+        createTable();
+        openConnection();
+        closeStatements();
+        closeConnection(true);
+    }
+
+    @Override
+    public Object getLatestSnapshot()
+    {
+        return null;
+    }
+
+
+    private void createTable()
+    {
+        try
+        {
+            String sql = "CREATE TABLE IF NOT EXISTS SNAPSHOT (\n"
+                    + "	id text NOT NULL PRIMARY KEY,\n"
+                    + "	game blob NOT NULL\n"
+                    + ");";
+            stmt = conn.createStatement();
+            stmt.execute(sql);
+            System.out.println("table created\n");
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeStatements();
+        }
+    }
+
+    private void openConnection()
+    {
+        conn = null;
         try
         {
             Class.forName("org.sqlite.JDBC");
@@ -39,21 +115,63 @@ public class SQLSnapshotDAO implements ISnapshotDAO
         }
     }
 
-    @Override
-    public void clear()
+    private void closeConnection(boolean commit)
     {
-
+        logger.entering("Database", "closeConnection");
+        try
+        {
+            if(conn!=null)
+            {
+                if(commit)
+                {
+                    System.out.println("Saving Database");
+                    conn.commit();
+                }
+                else
+                    conn.rollback();
+                conn.close();
+                conn = null;
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error occurred while closing connection");
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        logger.exiting("Database", "closeConnection");
     }
 
-    @Override
-    public void updateSnapshot(Object object)
-    {
 
+    private void closeStatements()
+    {
+        try
+        {
+            if(rs!=null)
+            {
+                rs.close();
+                rs = null;
+            }
+            if(pstmt!=null)
+            {
+                pstmt.close();
+                pstmt = null;
+            }
+            if(stmt!=null)
+            {
+                stmt.close();
+                stmt = null;
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error occurred while closing connection");
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            exit(0);
+        }
     }
 
-    @Override
-    public Object getLatestSnapshot()
+    public static void main(String[] args)
     {
-        return null;
+        new SQLSnapshotDAO().init();
     }
 }
