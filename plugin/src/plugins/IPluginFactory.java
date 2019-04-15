e605f0c64a3b51247a2e1a5e828467a436ec0ad2
Jake <ilovejava95@gmail.com>
2019-04-15 14:30:35 -0600

    .

--- plugin/src/plugins/IPluginFactory.java
+++
@@ -1,5 +0,0 @@
-package plugins;
-
-public interface IPluginFactory {
-    IDBPlugin getDBPluginInstance(String pluginDirectory, String pluginJarName, String pluginClassName)throws Exception ;
-}
--- plugin/src/plugins/JarTest.java
+++
@@ -1,9 +0,0 @@
-package plugins;
-
-public class JarTest
-{
-    public static void main(String args[])
-    {
-        System.out.println("a");
-    }
-}
